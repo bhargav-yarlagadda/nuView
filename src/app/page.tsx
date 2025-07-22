78d13d3cb477fce1fe5b782880ec3e4b98bf2f92
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2025-07-22 16:05:03 +0530

    feat.inngest AgenticAI kit setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 "use client"
 
+import { Input } from '@/components/ui/input';
 import { useTRPC } from '@/trpc/tRPC-wrapper'
 import { useMutation } from '@tanstack/react-query';
-import React from 'react'
+import React, { useState } from 'react'
 
 const page = () => {
+  const [value,setValue] = useState("")
+  const [result ,setResult ]= useState("")
   const trpc = useTRPC();
   const invoke  = useMutation(trpc.invoke.mutationOptions({}))
   return (
-    <div className='w-screen h-screen flex justify-center  items-center bg-black'>
+    <div className='w-screen h-screen flex flex-col justify-center  items-center bg-black'>
+      <Input className='max-w-40 my-12 bg-white' value={value} onChange={(e)=>setValue(e.target.value)}/>
       <button className='p-4 bg-sky-100 text-black rounded-full' onClick={()=>{
-        invoke.mutate({text:"John"})
+        invoke.mutate({value:value})
       }}>Invoke Backgroun Job</button>
     </div>
   )
--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,10 +1,19 @@
 import { inngest } from "./client";
 // inngest background jobs 
+import { Agent,  openai, createAgent } from "@inngest/agent-kit";
+
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+    const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer , you summarize into words.",
+      model: openai({ model: "gpt-4o",  }),
+    });
+    const {output} =await summarizer.run(`summarize the video : ${event.data.value}`)
+    return {
+      output: output  
+    }
   },
 );
